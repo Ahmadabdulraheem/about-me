59807162208a254a28602f38e297db06cde3c2b3
Ahmadabdulraheem <ahmad.abdulrahem@outlook.com>
2021-06-08 16:26:52 +0300

    adding more questions to about me website

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # about-me
-guessing game about me for user
+## guessing game about me for user
+----
+>A website allows the user to answer some questions about me then it will check them for him.
+
+we  sre going to use some statments in our code to make it more intreractive; such as: 
+* loops 
+* condtions
+
+The user will participate in feedback at the end of process.
+
--- js/app.js
+++ js/app.js
@@ -5,26 +5,28 @@ let userName = prompt('could you please tell me what is your name?');
 
 alert(' Welcome ' + userName + ' to our website');
 
-let study = prompt('do you think I am still studying in university?').toLowerCase();
+let study = prompt('do you think I am  graduated from the university?').toLowerCase();
 
 //console.log('Im graduted');
-
+let score = 0;
 if(study === 'yes' || study === 'y') {
   alert('you all right! I already finished my study ');
+   score = 1;
 } else if (study === 'no' || study === 'n') {
   alert('Actually I did 😇 ');
 } else {
   alert('Pease type (yes) or (no) or (y) or (n)');
 }
 
-let age = prompt('Am I underage?').toLowerCase();
+let age = prompt('Am I over 18 years?').toLowerCase();
 
 //console.log('Im 25');
 
 if(age === 'yes' || age === 'y') {
-  alert('Wrong I am over 18 ');
+  alert(' ofcourse Im not, we already said i finished the university '); 
+  score++;
 } else if (age === 'no' || age === 'n') {
-  alert('ofcourse Im not, we already said i finished the university ');
+  alert('Wrong I am over 18');
 } else {
   alert('Pease type (yes) or (no) or (y) or (n)');
 }
@@ -37,6 +39,7 @@ let field = prompt('can u tell if i am a programmer?').toLowerCase();
 
 if(field === 'yes' || field === 'y') {
   alert('correct! ^_^ ');
+  score++;
 } else if (field === 'no' || field === 'n') {
   alert('well Im just in the begining of my journy into programming,, so its yes ');
 } else {
@@ -53,6 +56,7 @@ let game = prompt('do you think I am a gamer?').toLowerCase();
 
 if(game === 'yes' || game === 'y') {
   alert('ofcourse ,, I love playng games');
+  score++;
 } else if (game === 'no' || game === 'n') {
   alert('I do ');
 } else {
@@ -68,10 +72,50 @@ let hobby = prompt('do I like reading?').toLowerCase();
 
 if(hobby === 'yes' || hobby === 'y') {
   alert('yes I do ');
+  score++;
 } else if (hobby === 'no' || hobby === 'n') {
   alert('Actually I do  ');
 } else {
   alert('Pease type (yes) or (no) or (y) or (n)');
 }
 
-alert ('thanks for your time ' + userName + 'see you soon 😍 ')
+
+
+//console.log('3');
+
+for(let i = 0; i < 4 ; i++) {
+  let countries = prompt('how many countries I have visted in my life?').toLowerCase();
+  console.log('3 countries');
+
+  if (countries == 3) {
+  alert ('  Yes,, its correct ' );
+  score++;
+  break;
+} else {
+  if (countries < 3 ) { alert ('too low')
+  
+ } 
+else { if (countries < 3) {
+  
+   } alert('too high'); }
+  
+ }
+}
+alert (' the correct answer was 3' );
+
+var favLanguage = ['1.Chinese', '2.english', '3.french', '4.Arabic', '5.spanish', '6.japanese', '7.russian'];
+
+for(let j = 1; j < 7 ; j++) {
+   var favLanguage = prompt('choose the language you think I like the most : 1.Chinese 2.English 3.French 4.Arabic 5.spanish 6.japanese 7.russian?').toLowerCase();
+  console.log('french');
+  if(favLanguage === 'french') { alert ('yes,, it is the correct answer' );
+  score++;
+break;
+    
+  } else {  alert ('can you please guess again? ');
+    
+  }
+}
+alert ('It was French ^_^ ');
+alert ('congrats!' + userName + 'your score is ' + score + ' out of 7' )
+alert ('thanks for your time ' + userName + 'see you soon 😍 ');
